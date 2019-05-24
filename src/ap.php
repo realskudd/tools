6905b7e600beec457a8d0d7e6a157dcfd4c552e4
Tim Garrison <tim@skudd.com>
2019-05-23 20:59:30 -0500

    Removed the namespace

--- src/ap.php
+++ src/ap.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace RealSkudd\tools;
-
-function ap($var)
-{
-    echo '<pre>' . print_r($var, true) . '</pre>';
+if (!function_exists('ap')) {
+    function ap($var)
+    {
+        echo '<pre>' . print_r($var, true) . '</pre>';
+    }
 }
